311a9122b72f35861be6300b6b01ae0e4487c76b
Lokesh-Balla <lokesh.balla123@gmail.com>
2019-07-15 12:30:47 +0530

    back to mysql

--- stackcopy/settings.py
+++ stackcopy/settings.py
@@ -81,22 +81,22 @@ WSGI_APPLICATION = 'stackcopy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'stackcopy',
-#         'HOST': 'localhost',
-#         'USER': "root",
-#         'PASSWORD': "traceon"
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'stackcopy',
+        'HOST': 'localhost',
+        'USER': "root",
+        'PASSWORD': "traceon"
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
