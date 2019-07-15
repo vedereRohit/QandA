951d01dbdba3436a84a95547043418f1a9d31987
Lokesh-Balla <lokesh.balla123@gmail.com>
2019-07-15 11:14:09 +0530

    changes to deploy on heroku

--- stackcopy/settings.py
+++ stackcopy/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -139,3 +140,6 @@ STATICFILES_DIRS = [
 ]
 
 INTERNAL_IPS = ['127.0.0.1']
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
